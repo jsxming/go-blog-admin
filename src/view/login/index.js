5652fc110c7ed974db2c29161743d4771a888648
TM <923727421@qq.com>
2020-11-13 19:46:53 +0800

    ~

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Form, Input, Button, Checkbox, } from 'antd';
 import { ArrowRightOutlined } from '@ant-design/icons';
-import {useHistory,} from 'react-router-dom';
+import { useHistory, } from 'react-router-dom';
 import './login.less';
 import API from '@/api/index';
 import { useDispatch } from 'react-redux';
-import {SET_TOKEN,SET_USER} from '@/redux/action-type';
-import {showInfo} from '@/util/index';
+import { SET_TOKEN, SET_USER } from '@/redux/action-type';
+import { showInfo } from '@/util/index';
 import { useForm } from 'antd/lib/form/Form';
-import {isValidValue} from '@/util/validator';
+import { isValidValue } from '@/util/validator';
 // const theme = require('@/theme/index');
 // theme.init();
 
@@ -18,23 +18,24 @@ export default function Login() {
     const dispatch = useDispatch();
     const [loginformRef] = useForm();
     const val = localStorage.getItem(catchKey);
-    if(isValidValue(val)){
+    if (isValidValue(val)) {
         loginformRef.setFieldsValue(JSON.parse(val));
     }
 
     const submit = values => {
-        if(values.remember){
-            localStorage.setItem(catchKey,JSON.stringify({account:values.account,password:values.password}));
+        if (values.remember) {
+            localStorage.setItem(catchKey, JSON.stringify({ account: values.account, password: values.password }));
         }
         API.login(values).then((res) => {
-            dispatch({type:SET_TOKEN,payload:res.token});
-            dispatch({type:SET_USER,payload:res.user});
-            API.queryUserAuth().then((res) => {
-                console.log(res);
-            }).catch((err) => {
+            dispatch({ type: SET_TOKEN, payload: res.token });
+            dispatch({ type: SET_USER, payload: res.user });
+            // API.queryUserAuth().then((res) => {
+            //     console.log(res);
+            // }).catch((err) => {
 
-            });
-            history.push('/home');
+            // });
+            console.log(123123, history);
+            history.push('/article');
         }).catch((e) => {
             showInfo(e.msg);
 
@@ -55,18 +56,18 @@ export default function Login() {
                         name="basic"
                         onFinish={submit}
                         size="large"
-                        style={{width:'100%'}}
+                        style={{ width: '100%' }}
                     >
                         <Form.Item
                             name="account"
-                            rules={[{ required: true, message: '请输入账号!'}]}
+                            rules={[{ required: true, message: '请输入账号!' }]}
                         >
                             <Input placeholder="账号" />
                         </Form.Item>
 
                         <Form.Item
                             name="password"
-                            rules={[{ required: true, message: '请输入密码!'}]}
+                            rules={[{ required: true, message: '请输入密码!' }]}
                         >
                             <Input.Password placeholder="密码" />
                         </Form.Item>
@@ -76,16 +77,16 @@ export default function Login() {
                                 htmlType="submit"
                                 type="primary"
                             >
-                                <ArrowRightOutlined style={{color:'#fff'}} />
+                                <ArrowRightOutlined style={{ color: '#fff' }} />
                             </Button>
                         </Form.Item>
                         <Form.Item
                             className="flex_center"
                             name="remember"
                             valuePropName="checked"
-                            wrapperCol={{offset: 0, span: 8}}
+                            wrapperCol={{ offset: 0, span: 8 }}
                         >
-                            <Checkbox style={{color:'#ACBAD4'}}>记住密码</Checkbox>
+                            <Checkbox style={{ color: '#ACBAD4' }}>记住密码</Checkbox>
                         </Form.Item>
                     </Form>
                 </div>
